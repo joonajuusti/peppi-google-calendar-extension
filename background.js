bc2ba81935b191af61feee3f3636d3cf004bc6e0
Joona Juusti <joona.juusti@gmail.com>
2019-02-02 17:49:18 +0200

    Make extension active when URL matces specified regex

--- background.js
+++ background.js
@@ -2,10 +2,12 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'opas.peppi.utu.fi'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
+        pageUrl: {
+          urlMatches: 'opas.peppi.utu.fi/fi/opintojakso/*|opas.peppi.utu.fi/en/course/*',
+          schemes: ['https'],
+        },
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 });
